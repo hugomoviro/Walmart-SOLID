99f1c450edeb450e358e68e91b5d38ff2e5e3cb3
Hugo Andres Martinez Guzman <hugo.martinez@moviro.mx>
2023-06-21 17:29:53 -0600

    first commit

---
+++ DAL/DALDBContext.cs
@@ -0,0 +1,29 @@
+﻿namespace WalmartApi.DAL
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using WalmartApi.Models;
+
+    public class DALDBContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DALDBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _configuration.GetConnectionString("connectionstring");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        // Agrega tus DbSet y configuraciones aquí
+        public DbSet<User> Users { get; set; }
+    }
+}
+
+   
+
+
